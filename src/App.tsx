cf1a892d56091ff5be75b121d0f8d8153bcc306e
Polina Yausheva <yausheva321@gmail.com>
2026-05-30 22:53:36 +0300

    start

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white p-8">
+      <h1 className="text-3xl font-bold text-green-400">Крипто Демотрейдинг</h1>
+      <p className="mt-4 text-gray-300">Проект в стадии разработки.</p>
+    </div>
+  );
+}
+
+export default App;
